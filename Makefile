07c149ac7932295dbaa9d89771b886ff87863543
rda <darnetremi@gmail.com>
2025-08-28 14:59:12 +0200

    change src

--- Makefile
+++ Makefile
@@ -1,8 +1,14 @@
 FILE = ./srcs/docker-compose.yml
 
 up :
-	docker-compose -f $(FILE) up  --build
+	docker-compose -f $(FILE) up -d --build
 
 clean : 
 	docker-compose -f $(FILE) stop
+fclean :
+	docker-compose -f $(FILE) stop \
+	&& sudo rm -rf /home/redarnet/data \
+	&& mkdir /home/redarnet/data \
+	&& docker system prune -a
+.PHONY: up clean fclean
 
--- srcs/requirements/mariadb/conf/mariadb-entrypoint.sh
+++ srcs/requirements/mariadb/conf/mariadb-entrypoint.sh
@@ -1,24 +1,13 @@
 #! /bin/bash
 
-# Start MariaDb
 service mysql start;
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${MARIADB_DATABASE}\`;"
 
-# Creation du user SQL_USER
 mysql -e "CREATE USER IF NOT EXISTS \`${MARIADB_USER}\`@'%' IDENTIFIED BY '${MARIADB_PASSWORD}';"
-
-# On donne tous les privileges a SQL_USER
 mysql -e "GRANT ALL PRIVILEGES ON \`${MARIADB_DATABASE}\`.* TO \`${MARIADB_USER}\`@'%' IDENTIFIED BY '${MARIADB_PASSWORD}';"
-
-# On remplace le root password par SQL_ROOT_PASSWORD
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIADB_ROOT_PASSWORD}';"
-
-# Rechargement de la table de droit
 mysql -e "FLUSH PRIVILEGES;"
-
-# On arrete MariaDB en utilisant root privileges
 mysqladmin -u root -p$MARIADB_ROOT_PASSWORD shutdown
 
-# On relance MariaDB
 exec mysqld_safe
 
